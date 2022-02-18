99aa45fe7868e9a1e3c6f88fa9169d4d117d6d85
liaojiangtu <42318168@qq.com>
2022-02-18 10:06:44 +0800

    feat:2022年02月18日更新，使用文档《附件三.商户客户端开发-IOS版开发.pdf》

--- Frameworks/ICBCPaySDK.framework/Headers/ICBCPaySDK.h
+++ Frameworks/ICBCPaySDK.framework/Headers/ICBCPaySDK.h
@@ -4,23 +4,50 @@
 //
 //  Created by wq on 16/9/26.
 //  Copyright © 2016年 wq. All rights reserved.
-// Modified by kfzx-hanw on 2020.9.2
+//
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-
+typedef NS_ENUM(NSInteger,PayWay) {
+    ICBCePay   = 22,
+    WeChatPay = 23,
+    AliPay        = 24
+};
 @protocol ICBCPaySDKDelegate <NSObject>
 @required
 /**
- *  调用sdk以后的结果回调
+ *  调用工银e支付以后的结果回调
  *
  *  @param resultCode 支付结果
  *  @param dic        回调的字典，参数中，ret_msg会有具体错误显示
  */
 - (void)paymentEndwithResultDic:(NSDictionary*)dic;
+
+///**
+// *  支付列表 用户选择支付结果回调
+// *
+// *  @param paymethod  支付方式@“epay”／@“wxpay”
+// */
+//- (NSDictionary*)epayOrWxpay:(NSString*)paymethod;
+
+/**
+ *  调用微信以后的结果回调
+ *
+ *  @param dic        回调的字典，参数中，errCode会有具体错误显示
+ */
+-(void)wxPayEndwithResultDic:(NSDictionary*)dic;
+///支付宝支付结果回掉
+-(void)aliPayEndwithResultDic:(NSDictionary*)dic;
+
+///**
+//    巨富通
+// *  支付宝遮罩关闭
+// *
+// */
+//- (void)sdkMaskGotoClose;
 @end
 
-
+/** 单例作为微信支付的代理 */
 @interface ICBCPaySDK : NSObject
 
 /**
@@ -32,6 +59,9 @@
 
 /** 代理 */
 @property (nonatomic, weak) id<ICBCPaySDKDelegate>  sdkDelegate;
+//巨富通
+///** 遮罩 */
+//@property (nonatomic, strong) UIView *blackMask;
 
 /**
  *  工行支付 支付接口
@@ -41,15 +71,52 @@
  */
 - (void)presentICBCPaySDKInViewController: (UIViewController *)viewController
                           andTraderInfo: (NSDictionary *)traderInfo;
+
+/**
+ *  微信支付 支付接口
+ *
+ *  @param viewController 推出微信支付界面的ViewController
+ *  @param traderInfo     交易信息
+ */
+-(void)presentWeChatInViewController:(UIViewController *)viewController
+                       andTraderInfo:(NSDictionary *)traderInfo;
+/**
+ *  支付列表接口 支付接口
+ *
+ *  @param viewController 推出支付列表界面的ViewController
+ *  @param traderInfo     交易信息(现在仅有商户ID)
+ */
+-(void)presentPayListViewController:(UIViewController *)viewController
+                            showDic:(NSDictionary *)merID
+                      addTraderInfo:(NSDictionary *)traderInfo
+                     addTraderInfo2:(NSDictionary *)traderInfo2;
+/**
+ *  支付列表接口 支付完成后退出sdk支付列表页返回商户APP接口 只需要调用支付列表的商户实现
+ *
+ */
+- (void)exitICBCPaySDK;
+/**
+ *  支付宝支付 接口
+ *
+ *  @param viewController 推出支付列表界面的ViewController
+ *  @param traderInfo     交易信息(现在仅有商户ID)
+ */
+-(void)presentAlipaySDKViewController:(UIViewController *)viewController
+                        andTraderInfo:(NSDictionary *)traderInfo;
 /**
  
- *  支付回跳App
+ *  e支付从手机银行回跳商户App
  *
- *  @param url         支付完成回调的URL信息
+ *  @param url         支付完成，手机银行跳回商户App时回调的URL信息
  */
 - (void)ICBCResultBackWithUrl: (NSURL *)url;
-
-
+/**
+ 
+ *  支付宝回跳商户App
+ *
+ *  @param url         支付完成，支付宝跳回商户App时回调的URL信息
+ */
+- (void)aliPayResultBackWithUrl: (NSURL *)url;
 
 /**
  
@@ -59,9 +126,9 @@
 - (NSString *)getVersion;
 
 
-// 测试环境，请配置urlListmain
+/** 测试环境，请配置urlListmain 和urlPortal */
 
-@property (nonatomic, strong) NSString *urlListMain;//支付平台地址
-
+@property (nonatomic, strong) NSString *urlListMain;//客户端支付列表，支付平台地址
+@property (nonatomic, strong) NSString *urlPortal;//跳转h5地址，手机银行地址
 
 @end
